effcb144cf0dae796ca9c3b16eb5090de8d702d8
juliusstr <julius@struwing.dk>
2023-10-09 16:49:56 +0200

    typedef

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef long word;
+typedef int hWord;
+typedef char byte;
+typedef long uWord;
+typedef int uHWord;
+typedef char uByte;
+
+
 int main(){
+    word ha = 0xaaaaaaaa;
 	printf("hello world\n");
 }
