e223d47d09d8af7f5cfa350d76e437bf525b3889
BertramDTU <s205121@student.dtu.dk>
2023-11-23 20:56:43 +0100

    RunTestFiles

--- main.c
+++ main.c
@@ -8,15 +8,36 @@ int main(int argc, char* argv[]){
     else {
         perror("getcwd() error");
     }*/
-    #ifdef DEBUG
+/*    #ifdef DEBUG
     srand(time(NULL));
     DEBUG_PRINT("Executing unit tests\n");
     ExecuteAllTests();
     #endif
-
-    readFile("../RiscVSim/addlarge.bin");
+    */
+    char filePath[50];
+    FILE *fpTests;
+    fpTests = fopen("tests/tests.txt","r");
+    while (1)
+    {
+    char testfile[]= "../RiscVSim/";
+        fscanf(fpTests,"%s",filePath);
+        if(feof(fpTests)){
+            break;
+        }
+    strcat(testfile,filePath);
+    
+    printf("\n Running test located at : %s\n \n",testfile);
+    
+    readFile(testfile);
     InternalProcessorMemory ipm;
     ipm.pc = getInitPC();
     Compute(&ipm);
+    PrintRegisters(&ipm);
+    }
+
+//    readFile("../RiscVSim/addlarge.bin");
+//    InternalProcessorMemory ipm;
+//    ipm.pc = getInitPC();
+ //   Compute(&ipm);
     //PrintRegisters(&ipm);
 }
