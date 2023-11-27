f56c6655f6d043c21648a196178e4284b25bc235
juliusstr <julius@struwing.dk>
2023-11-27 10:55:41 +0100

    setup for demo

--- main.c
+++ main.c
@@ -6,24 +6,35 @@ int main(int argc, char* argv[]){
     DEBUG_PRINT("Executing unit tests\n");
     ExecuteAllTests();
     #endif
-    char filePath[50];
-    FILE *fpTests;
-    fpTests = fopen("tests/tests.txt","r");
-    while (1)
-    {
-    char testfile[]= "../RiscVSim/";
-        fscanf(fpTests,"%s",filePath);
-        if(feof(fpTests)){
-            break;
+
+    if(argc == 1) {
+        char filePath[50];
+        FILE *fpTests;
+        fpTests = fopen("tests/tests.txt", "r");
+        while (1) {
+            char testfile[] = "../RiscVSim/";
+            fscanf(fpTests, "%s", filePath);
+            if (feof(fpTests)) {
+                break;
+            }
+            strcat(testfile, filePath);
+
+            printf("\n Running test located at : %s\n \n", testfile);
+
+            readFile(testfile);
+            InternalProcessorMemory ipm;
+            ipm.pc = getInitPC();
+            Compute(&ipm);
+            PrintRegisters(&ipm);
         }
-    strcat(testfile,filePath);
-
-    printf("\n Running test located at : %s\n \n",testfile);
-
-    readFile(testfile);
-    InternalProcessorMemory ipm;
-    ipm.pc = getInitPC();
-    Compute(&ipm);
-    PrintRegisters(&ipm);
+    } else{
+        for (int i = 1; i < argc; ++i) {
+            printf("\n Running test located at : %s\n \n", argv[i]);
+            readFile(argv[i]);
+            InternalProcessorMemory ipm;
+            ipm.pc = getInitPC();
+            Compute(&ipm);
+            PrintRegisters(&ipm);
+        }
     }
 }
